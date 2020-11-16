b61a6b5d780d001589d679b85fd171c22f2ce790
pirateontheseas <56448070+pirateontheseas@users.noreply.github.com>
2020-11-15 18:06:35 -0600

    commit2

--- todo1/asgi.py
+++ todo1/asgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todowoo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo1.settings')
 
 application = get_asgi_application()
 
--- todo1/wsgi.py
+++ todo1/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todowoo.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todo1.settings')
 
 application = get_wsgi_application()
 
